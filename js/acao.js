501af0de8450353ea2c21941f763a42bca765e8f
Endriw98 <109055688+Endriw98@users.noreply.github.com>
2023-12-24 18:08:04 -0300

    Update acao.js

--- js/acao.js
+++ js/acao.js
@@ -84,36 +84,50 @@ function retornar() {
             titulo.textContent = "Sabe o quanto eu gosto de você?"
             paragrafo.textContent = "Bom, quando a gente terminou, de início não caiu a ficha. Mas depois parei para refletir um pouco, de que não posso forçar algo no qual você não estava bem. Eu estaria sendo ignorante, e narcisista forçar a você a ficar com alguém, no qual não confia. Então resolvi seguir em frente.";
             img.style.backgroundImage = "url('img/solidao.jpg')";
+            button1.style.display = "block";
+            button1.disabled = false;
         } else if (i == 2) {
             scrollToTop();
             titulo.textContent = "Pensava em você todo dia.";
             paragrafo.textContent = "Dias se passavam, e você não saía da minha mente, pensava no que eu fiz de errado que fez você perder a confiança em mim. E muitas vezes, queria que esse término fosse temporário e que você voltaria para mim, e quanto mais demorava, menor a esperança ficava.";
             img.style.backgroundImage = "url('img/solidao.jpg')";
+            button1.style.display = "block";
+            button1.disabled = false;
         } else if (i == 3) {
             scrollToTop();
             titulo.textContent = "Seguindo em frente.";
             paragrafo.textContent = "Todas as vezes que eu via a sua foto, e a foto de nós dois que ainda tenho, me perco nos momentos em que passamos. Porém vi que não podia ficar assim, e apaguei todas as fotos do celular, deixando guardadas na nuvem, de forma arquivada."
             img.style.backgroundImage = "url('img/coracaopartido.jpg')";
+            button1.style.display = "block";
+            button1.disabled = false;
         } else if (i == 4) {
             scrollToTop();
             titulo.textContent = "Não superei...";
             paragrafo.textContent = "Mas sempre voltava a ver as suas fotos, e sempre ficava maravilhado e pensativo, imaginando como seria se eu ainda estivesse contigo."
             img.style.backgroundImage = "url('img/coracaopartido.jpg')";
+            button1.style.display = "block";
+            button1.disabled = false;
         } else if (i == 5) {
             scrollToTop();
             titulo.textContent = "Amo você demais.";
             paragrafo.textContent = "Cada dia que passei contigo foi único, e esses dias jamais serão esquecidos.";
             img.style.backgroundImage = "url('img/02.jpg')";
+            button1.style.display = "block";
+            button1.disabled = false;
         } else if (i == 6) {
             scrollToTop();
             titulo.textContent = "Cuide-se";
             paragrafo.textContent = "Se a vontade de voltar bater, é certeza que eu voltaria contigo. Mas tudo depende de você, e o que eu mais queria, na época, era servir de apoio a você quando passasse por turbulências na sua vida.";
             img.style.backgroundImage = "url('img/01.jpg')";
+            button1.style.display = "block";
+            button1.disabled = false;
         } else if (i == 7) {
             scrollToTop();
             titulo.textContent = "Feliz Natal!";
             paragrafo.textContent = "De certa forma, queria passar o Natal e o Reveillon contigo, mas como não será possível, desejo a você feliz Natal e um próspero Ano Novo, que você realize seus sonhos, e lhe desejo muita felicidade.";
             img.style.backgroundImage = "url('img/03.jpg')";
+            button1.style.display = "block";
+            button1.disabled = false;
         } else if (i == 8) {
             scrollToTop();
             titulo.textContent = "Amo você!";
